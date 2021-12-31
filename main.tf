4ee64f2ce16ef61099a7db097103b8d0972a12be
barbariska108 <boris11001@gmail.com>
2021-12-31 11:03:45 +0200

    now provider and backend in separate file

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-  backend "s3" {
-    bucket = "awesome-terraform-state-bucket"
-    key    = "env/staging"
-  }
-  required_version = ">= 0.15"
-}
-
-provider "aws" {
-  region = local.region
-  default_tags {
-    tags = local.common_tags
-  }
-}
-
 module "awesome_vpc" {
   source      = "./modules/awesome_vpc"
   name        = local.vpc_name
---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "awesome-terraform-state-bucket"
+    key    = "env/staging"
+  }
+  required_version = ">= 0.15"
+}
+
+provider "aws" {
+  region = local.region
+  default_tags {
+    tags = local.common_tags
+  }
+}
